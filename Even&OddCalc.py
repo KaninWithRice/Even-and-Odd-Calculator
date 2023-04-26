56066761662e74224f541adee61203871e494958
KaninWithRice <mishalnorona014@gmail.com>
2023-04-26 13:58:38 +0800

    User's Input Added

--- Even&OddCalc.py
+++ Even&OddCalc.py
@@ -5,7 +5,15 @@ print("WELCOME TO THE EVEN & ODD CALCULATOR".center(40," ") )
 print("By: Mishal Noroña".center(40," ") )
 
 # Ask user for numbers of input
+num_input = int(input("Enter how many you want to input: "))
+user_input_list = open("user_input.txt","w")
+
 # Ask user to enter numbers
+numbers = []
+for i in range(1, 1 + num_input):
+    user_input = (input("Enter a number: \t"))
+    numbers.append(user_input)
+
 # Create a text file for user's input
 # Create a Formula for Separating Even and Odd
 # Add Loading Time

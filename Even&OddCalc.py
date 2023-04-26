9665134dc1358bd7d44e68c3a1d3842a55e5e380
KaninWithRice <mishalnorona014@gmail.com>
2023-04-26 13:56:10 +0800

    Pseudocode

---
+++ Even&OddCalc.py
@@ -0,0 +1,11 @@
+# Mohammad Mishal S. Noroña | BSCPE 1-5 | Porblem 1 Even and Odd
+# Add introduction
+# Ask user for numbers of input
+# Ask user to enter numbers
+# Create a text file for user's input
+# Create a Formula for Separating Even and Odd
+# Add Loading Time
+# Ceate a Even Text File
+# Print Even List
+# Ceate a Odd Text File
+# Print Odd List

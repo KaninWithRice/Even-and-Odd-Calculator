5f4249ddd9cdf92b669ebb410f13588b2ea47195
KaninWithRice <mishalnorona014@gmail.com>
2023-04-26 13:57:13 +0800

    Introduction

--- Even&OddCalc.py
+++ Even&OddCalc.py
@@ -1,5 +1,9 @@
 # Mohammad Mishal S. Noroña | BSCPE 1-5 | Porblem 1 Even and Odd
 # Add introduction
+print("")
+print("WELCOME TO THE EVEN & ODD CALCULATOR".center(40," ") )
+print("By: Mishal Noroña".center(40," ") )
+
 # Ask user for numbers of input
 # Ask user to enter numbers
 # Create a text file for user's input
